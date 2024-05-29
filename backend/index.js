e2222accfb6309d86fae0ec5442cb9134df65e04
koding88 <duongngocanh2k03@gmail.com>
2024-05-29 16:45:33 +0700

    Connetc to Database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
+// Configuring dotenv to use the .env file
 dotenv.config();
+
+// Connect to MongoDB
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    }).catch((error) => {
+        console.log(error);
+    });
 
 const app = express();
 
